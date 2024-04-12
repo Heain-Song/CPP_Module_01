fec3eb51f02fcdb05db9c2e73b35029c813b0913
Hea In Song <heain.song328@gmail.com>
2024-04-12 12:46:42 +0200

    ex04 done

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hesong <hesong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 22:04:42 by hesong            #+#    #+#             */
-/*   Updated: 2024/04/11 23:26:32 by hesong           ###   ########.fr       */
+/*   Updated: 2024/04/12 12:46:04 by hesong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 #include <string>
 #include <fstream>
 
-void	replaceString(std::string &line, const std::string &s1, const std::string &s2)
+void	replaceString(std::string &buffer, const std::string &s1, const std::string &s2)
 {
 	size_t pos = 0;
-	while ((pos = line.find(s1, pos)) != std::string::npos)
+	while ((pos = buffer.find(s1, pos)) != std::string::npos) // if found
 	{
-		line = line.substr(0, pos) + s2 + line.substr(pos + s1.length());
+		buffer = buffer.substr(0, pos) + s2 + buffer.substr(pos + s1.length());
 		pos += s2.length();
 	}
 }
@@ -32,36 +32,33 @@ int main(int argc, char **argv)
 		std::string s2 = argv[3];
 		std::string outputFilename = filename + ".replace";
 
-		std::ifstream inputFile(filename.c_str()); // 파일 입력
-		std::ofstream outputFile(outputFilename.c_str()); // 파일 출력
+		std::ifstream inputFile(filename.c_str()); // open the file to read. c_str() to convert the string to an array of characters
+		std::ofstream outputFile(outputFilename.c_str()); // open the file to write
 
 		if (!inputFile.is_open())
 		{
-			std::cerr << "Error: can't open the input file" << std::endl;
+			std::cout << "Error: can't open the input file" << std::endl;
 			return (1);
 		}
 		if (!outputFile.is_open())
 		{
-			std::cerr << "Error: can't open the output file" << std::endl;
+			std::cout << "Error: can't open the output file" << std::endl;
 			return (1);
 		}
 
-		std::string line;
-		while (std::getline(inputFile, line))
+		std::string buffer;
+		while (std::getline(inputFile, buffer)) // Read each line from the file
 		{
-			replaceString(line, s1, s2);
-			outputFile << line << std::endl;
+			replaceString(buffer, s1, s2);
+			outputFile << buffer << std::endl;
 		}
 		inputFile.close();
 		outputFile.close();
-		std::cout << "Completed: in " << outputFilename << std::endl;
+		std::cout << "Success: Replacement comepleted" << std::endl;
 		return (0);
 	}
 	else
-	{
-		std::cerr << "Error: Invalid number of arguments" << std::endl;
-		return (1);
-	}
+		std::cout << "Error: Invalid number of arguments" << std::endl;
 	return (0);
 }
 
