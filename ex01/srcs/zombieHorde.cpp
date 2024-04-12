ec4bf581f0150623cb5eb4e709c5ae8f3ae2e9e8
Hea In Song <heain.song328@gmail.com>
2024-04-12 16:08:20 +0200

    ex01 final

--- ex01/srcs/zombieHorde.cpp
+++ ex01/srcs/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: hesong <hesong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 17:26:13 by hesong            #+#    #+#             */
-/*   Updated: 2024/04/11 19:26:09 by hesong           ###   ########.fr       */
+/*   Updated: 2024/04/12 16:07:06 by hesong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ Zombie* zombieHorde( int N, std::string name )
 {
 	if (N <= 0)
 		return (NULL);
-	Zombie* zombies = new Zombie[N];
+	Zombie *zombies = new Zombie[N];
 	for (int i = 0; i < N; i++)
 		zombies[i].setName(name);
 	return (zombies);
