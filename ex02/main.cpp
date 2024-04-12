5fc5f52fa318958e5da39119d647719fbfc30f8b
Hea In Song <heain.song328@gmail.com>
2024-04-12 16:17:14 +0200

    ex02 final

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hesong <hesong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 20:22:40 by hesong            #+#    #+#             */
-/*   Updated: 2024/04/11 20:22:41 by hesong           ###   ########.fr       */
+/*   Updated: 2024/04/12 16:16:47 by hesong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 
 int main(void)
 {
-	std::string	str = "HI THIS IS BRAIN";
-	std::string	*stringPTR = &str;
-	std::string &stringREF = str;
+	std::string	stringORG = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &stringORG;
+	std::string &stringREF = stringORG;
 
-	std::cout << "Memory address of str : " << &str << std::endl;
+	std::cout << "Memory address of stringORG : " << &stringORG << std::endl;
 	std::cout << "Memory address of stringPRT : " << stringPTR << std::endl;
 	std::cout << "Memory address of stringREF : " << &stringREF << std::endl;
-	std::cout << "Value in str : " << str << std::endl;
+	std::cout << "Value in stringORG : " << stringORG << std::endl;
 	std::cout << "Value in stringPRT : " << *stringPTR << std::endl;
 	std::cout << "Value in stringREF : " << stringREF << std::endl;
 }
